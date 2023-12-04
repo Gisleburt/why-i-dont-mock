0fdefbcc0777bd08e1c95c9bc316cc2637e4fd85
Daniel Mason <daniel.mason@beamery.com>
2023-12-04 16:41:22 +0000

    fancier name

--- src/slides/intro.rs
+++ src/slides/intro.rs
@@ -4,8 +4,8 @@ use dioxus::prelude::*;
 pub fn Intro(cx: Scope) -> Element {
     cx.render(rsx!(
         h1 { "Why I don't mock" }
-        h2 { "Daniel // Mason" }
-        Notes { 
+        h2 { class: "daniel", "Daniel " span { "// Mason" } }
+        Notes {
             p { "Hi, in case you don't know I'm Daniel and I love testing" }
             p { "But testing interconnected things gets complex" }
             p { "So I want to show you how I manage that complexity" }
